117f8841b8d555bfb316babb574f5ecf09ae91b7
Amir Bavand <amirhossein.bavand@gmail.com>
2024-09-27 07:17:07 -0400

    remove test env environment from end to end tests

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -3,27 +3,23 @@ package e2e
 import (
 	"context"
 	"fmt"
-	"path/filepath"
+	"os/exec"
 	"testing"
 
 	kumquatv1beta1 "kumquat/api/v1beta1"
-
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"kumquat/test/utils"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/e2e-framework/support/kind"
 )
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+//var cfg *rest.Config
+
+// var k8sClient client.Client
+// var testEnv *envtest.Environment
 var kindClusterName = "kind"
 var kindCluster *kind.Cluster
 
@@ -42,33 +38,18 @@ var _ = BeforeSuite(func() {
 	_, err := kindCluster.SetDefaults().Create(context.TODO())
 	Expect(err).NotTo(HaveOccurred())
 
-	var useExistingCluster bool = true
-	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
-		BinaryAssetsDirectory: filepath.Join("..", "..", "bin", "k8s", "1.30.0-linux-arm64"),
-		UseExistingCluster:    &useExistingCluster,
-	}
-
-	cfg, err = testEnv.Start()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(cfg).NotTo(BeNil())
-
 	err = kumquatv1beta1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-	Expect(err).NotTo(HaveOccurred())
-	Expect(k8sClient).NotTo(BeNil())
+	cmd := exec.Command("kubectl", "create", "ns", "templates")
 
-	namespace := corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "templates"}}
-	err = k8sClient.Create(context.Background(), &namespace)
+	_, err = utils.Run(cmd)
+	Expect(err).NotTo(HaveOccurred())
+
 	Expect(err).NotTo(HaveOccurred())
 })
 
 var _ = AfterSuite(func() {
 	err := kindCluster.Destroy(context.TODO())
-	Expect(err).NotTo(HaveOccurred())
-	By("tearing down the test environment")
-	err = testEnv.Stop()
 	Expect(err).NotTo(HaveOccurred())
 })
