cbcb7982b16ecfd987684006db70d8bd54e5cc2c
Katrina Ronquillo <katrina.ronq@gmail.com>
2025-06-20 16:21:49 -0400

    ✅ Report to Fern after E2E tests

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -3,21 +3,27 @@ package e2e
 import (
 	"context"
 	"fmt"
+	"os"
 	"os/exec"
 	"testing"
 
 	kumquatv1beta1 "kumquat/api/v1beta1"
 	"kumquat/test/utils"
 
+	"github.com/guidewire-oss/fern-ginkgo-client/pkg/client"
 	. "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/e2e-framework/support/kind"
 )
 
-var kindClusterName = "kind"
-var kindCluster *kind.Cluster
+var (
+	kindClusterName = "kind"
+	kindCluster     *kind.Cluster
+	fernClient      *client.FernApiClient
+)
 
 func TestE2E(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -48,4 +54,21 @@ var _ = BeforeSuite(func() {
 var _ = AfterSuite(func() {
 	err := kindCluster.Destroy(context.TODO())
 	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = ReportAfterSuite("", func(report Report) {
+	projectID := os.Getenv("E2E_FERN_PROJECT_ID")
+
+	fernReporterBaseURL := "https://fern-reporter.int.ccs.guidewire.net/"
+	if os.Getenv("FERN_REPORTER_BASE_URL") != "" {
+		fernReporterBaseURL = os.Getenv("FERN_REPORTER_BASE_URL")
+	}
+
+	if os.Getenv("REPORT_TO_FERN") == "true" {
+		fernClient = client.New(projectID, client.WithBaseURL(fernReporterBaseURL))
+		err := fernClient.Report(report)
+		gomega.Expect(err).ToNot(gomega.HaveOccurred(), "Unable to push report to Fern %v", err)
+	} else {
+		fmt.Println("Skipping report to Fern as REPORT_TO_FERN is set to false")
+	}
 })
